7c7f24542711cb046ea190478827aa36082429d6
Michael Friendly <friendly@yorku.ca>
2025-11-08 14:59:27 -0500

    Initial commit

---
+++ R/colorize.R
@@ -0,0 +1,67 @@
+#' Render text in color for Markdown / Quarto documents
+#'
+#' The `colorize()` function renders text in color for Markdown / Quarto documents. For documents to be rendered in HTML,
+#' it uses a CSS `<span>`; for documents to be converted to LaTeX and rendered as PDF, it
+#' uses `\textcolor{}{}` from the [`xcolor`](https://ctan.org/pkg/xcolor) package.
+#' A companion function, `colorize_bg()` does the same, but uses the specified color for the background.
+#'
+#' @details
+#'
+#' The names of colors
+#' Note that a color name not defined in the `xcolor` package will trigger a LaTeX error.
+#' e.g., `darkgreen` is not defined but you can use:
+#'    `\definecolor{darkgreen}{RGB}{1,50,32}` in a document to be rendered to PDF.
+#'
+#' For inline text, in running text, you can use:
+#' \preformatted{
+#'  `r colorize("Gentoo", "orange")` and  `r colorize("Adelie", "purple")` are Penguins.
+#'  The `r colorize("red")` points and the `r colorize("blue")` points are nice
+#'  }
+#'
+#'  In a chunk, you can also define variables with the names of colors, for ease of use:
+#'
+#'  \preformatted{
+#'  red <- colorize('red')
+#'  pink <- colorize("pink")
+#'  blue <- colorize('blue')
+#'  green <- colorize("green")
+#'  yellow <- colorize("yellow")
+#'  lightgreen <- colorize("lightgreen")
+#'  darkgreen <- colorize("darkgreen")
+#'  brown <- colorize("brown", "brown4")
+#'  }
+#'
+#'
+#' @param text  Text to display, a character string
+#' @param color Color to use, a valid color designation; if missing, use `text` as the color
+#'
+#' @return A character string with color-encoded text
+#' @export
+#'
+#' @examples
+#'
+colorize <- function(text, color) {
+  if (missing(color)) color <- text
+  if (knitr::is_latex_output()) {
+    sprintf("\\textcolor{%s}{%s}", color, text)
+  } else if (knitr::is_html_output()) {
+    sprintf("<span style='color: %s;'>%s</span>", color, text)
+  } else text      # Fallback for other formats
+}
+
+# Define some color names for use in figure captions.
+# use as:
+#    #| fig-cap: !expr glue::glue("Some points are {red}, some are  {blue}, some are {green}")
+
+# perhaps better to color the background
+colorize_bg <- function(text, bgcolor) {
+  if (missing(bgcolor)) bgcolor <- text
+  if (knitr::is_latex_output()) {
+    sprintf("\\colorbox{%s}{%s}", bgcolor, text)
+  } else if (knitr::is_html_output()) {
+    sprintf("<span style='background-color: %s;'>%s</span>", bgcolor, text)
+  } else {
+    text # Fallback for other formats
+  }
+}
+
---
+++ extra/create_package.R
@@ -0,0 +1,18 @@
+# create the colorize package
+# From: https://kbroman.org/AdvData/18_rpack_demo.html
+#
+
+library(usethis)
+
+# package skeleton
+create_package("C:/Dropbox/R/projects/colorize")
+
+use_git()
+
+use_mit_license("Michael Friendly")
+
+use_readme_rmd()
+
+
+# create GitHub repo
+use_github()
